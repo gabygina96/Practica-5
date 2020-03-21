7ab39b416d6977d87a3d88488feebaa9f6039b2e
Gabriela <gagegas@hotmail.com>
2020-03-20 21:14:10 -0600

    Agregar practica 5

---
+++ Practica 5/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    private Animator enemyAnimator;
+    private NavMeshAgent enemyAgent;
+    private Transform playerTransform;
+    private Shooter daño;
+    private LifeBar player;
+    public int vida = 3;
+    public int muerte = 0;
+    public ParticleSystem particule;
+    public ParticleSystem particleDead;
+
+    void Start()
+    {
+        enemyAnimator = GetComponent<Animator>();
+        enemyAgent = GetComponent<NavMeshAgent>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        particleDead.Stop();
+    }
+
+
+    void Update()
+    {
+        enemyAgent.SetDestination(playerTransform.position);
+        Debug.Log("Distance to player: " +
+            enemyAgent.remainingDistance);
+        if (enemyAgent.remainingDistance <= 1f &&
+            enemyAgent.hasPath)
+        {
+            enemyAnimator.SetFloat("Speed", 0f);
+            enemyAnimator.SetBool("Punch", true);
+        }
+        else
+            enemyAnimator.SetFloat("Speed", enemyAgent.speed);
+    }
+
+     void OnTriggerEnter(Collider colision){
+        if (colision.tag == "Bullet") {
+            muerte += 1;
+            Destroy(colision.gameObject);
+            if(vida == muerte){
+                StartCoroutine ("removeEnemy");
+            }
+        }
+    }
+     IEnumerator removeEnemy(){
+        enemyAnimator.SetTrigger("Dead");
+        particule.Stop();
+        particleDead.Play();
+        yield return new WaitForSeconds(5f);
+        Destroy(gameObject);
+    }
+}
---
+++ Practica 5/Assets/Scripts/EnemyGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public Vector2 spawnRange;
+    public Transform posEnemy;
+
+    public Transform posEnemy2;
+    private GameObject tmpEnemy;
+
+    private GameObject tmpEnemy2;
+
+    void Start()
+    {
+        // InvokeRepeating("SpawnEnemy",0f,3f);
+        StartCoroutine("spawnEnemies");
+    }
+
+    // void SpawnEnemy()
+    // {
+    //     Instantiate(enemyPrefab, 
+    //      new Vector3(Random.Range(
+    //                  -spawnRange.x, spawnRange.x),
+    //                 0,
+    //                 Random.Range(
+    //                  -spawnRange.y, spawnRange.y)),
+    //                 Quaternion.identity);
+    // }
+
+    IEnumerator spawnEnemies()
+    {
+        tmpEnemy = Instantiate(enemyPrefab, posEnemy.position, Quaternion.identity);
+        tmpEnemy2 = Instantiate(enemyPrefab, posEnemy2.position, Quaternion.identity);
+        yield return new WaitForSeconds(15f);
+        StartCoroutine("spawnEnemies");
+    }
+}
